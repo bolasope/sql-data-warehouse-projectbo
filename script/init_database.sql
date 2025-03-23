81a111eaaafc52b94b248d9434648dcb59d20315
bolasope <95140028+bolasope@users.noreply.github.com>
2025-03-23 22:38:03 +0000

    Update init_database.sql

--- script/init_database.sql
+++ script/init_database.sql
@@ -1,3 +1,13 @@
+=====================================================
+  --Create Database and Schemas
+====================================================
+
+Script Purpose:
+  This script creates a new database named 'DataWarehouse' 
+  This script set up three schemas within the database; bronze, silver and gold.
+
+
+
 -- Created Database right click Database > Create New Database
 
 -- Create Schemas
